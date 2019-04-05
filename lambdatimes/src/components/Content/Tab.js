33ac1cd881440effe75b664321cb617cd334cdaa
sriley <sarahriley.dev@gmail.com>
2019-04-05 13:18:48 -0400

    added PropTypes

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
 const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
@@ -22,5 +23,9 @@ const Tab = props => {
 };
 
 // Make sure you include PropTypes on your props.
+
+Tab.propTypes = {
+  tabData: PropTypes.string
+};
 
 export default Tab;
