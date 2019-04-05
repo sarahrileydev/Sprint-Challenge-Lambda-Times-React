e7b605ba160cebfbcda928e0680baf3c26263b46
sriley <sarahriley.dev@gmail.com>
2019-04-05 13:45:51 -0400

    Header refactored

--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -1,7 +1,11 @@
 import React from 'react';
-
+import styled from "styled-components";
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
+
+const HeaderDiv = styled.div`
+  padding: 10px 0 0 10px;
+`;
 
 const TopBar = () => {
   return (
